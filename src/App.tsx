c23446b43a024c10542b3a8b08363cb051dfa398
Ian Hansson <ian.hansson@gmail.com>
2020-12-06 14:57:27 +0000

    Styled components testing

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Switch, Link, Route } from "react-router-dom";
 import { PageA } from "./components/PageA";
 import { PageB } from "./components/PageB";
 import { PageC } from "./components/PageC";
+import { PageD } from "./components/PageD";
 
 function App() {
   return (
@@ -14,10 +15,13 @@ function App() {
         <Link to="/page-b">B</Link>
         <br />
         <Link to="/page-c">C</Link>
+        <br />
+        <Link to="/page-d">D</Link>
       </header>
       <Switch>
         <Route exact path="/page-b" children={<PageB />} />
         <Route exact path="/page-c" children={<PageC />} />
+        <Route exact path="/page-d" children={<PageD />} />
         <Route exact path="/" children={<PageA />} />
       </Switch>
     </div>
--- src/components/PageD.tsx
+++ src/components/PageD.tsx
@@ -1,9 +1,37 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  color: red;
+`;
+
+const SubTitle = styled(Title)`
+  color: ${(props) => props.color};
+`;
+
+const Custom: React.FunctionComponent<{ className?: string }> = ({
+  className = "",
+}) => {
+  return <div {...{ className }}>I am custom</div>;
+};
+
+const CustomRed = styled(Custom)`
+  color: red;
+  @media (min-width: 768px) {
+    color: yellow;
+  }
+`;
+
 export const PageD = () => {
   return (
     <div>
       <h1>Page D</h1>
       <section>
         <h2>Styled Component Testing</h2>
+        <Title>Test Me</Title>
+        <SubTitle color="green">Test Me</SubTitle>
+        <Custom />
+        <CustomRed />
       </section>
     </div>
   );
